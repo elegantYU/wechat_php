bbceb131a16592fcb4d1d2b34138ced056177ad4
most handsome man in the world <845384559@qq.com>
2018-07-11 10:15:27 +0800

    wechat

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+// 引入jssdk.php
+require_once "jssdk.php";
+$jssdk = new JSSDK("wx84e111b462a08abb", "d1572926dee5fd494cff00b60467dc4c");
+// 获取signPackage数组
+$signPackage = $jssdk->GetSignPackage();
+// 得到 JSSDK文件中的$signPackage
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>请求示例</title>
+    <!-- 使用微信接口js -->
+    <script src="https://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+    <script>
+        //  $signPackage 这个包里的数据需要加入配置项
+        wx.config({
+            // 默认debug为false，开发调试建议开启
+            debug: false,
+            appId: '<?php echo $signPackage["appId"];?>',
+            timestamp: '<?php echo $signPackage["timestamp"];?>',
+            nonceStr: '<?php echo $signPackage["nonceStr"];?>',
+            signature: '<?php echo $signPackage["signature"];?>',
+            // 将要使用的接口 写入下面数组
+            jsApiList: ['onMenuShareTimeline','onMenuShareAppMessage']
+        });
+        wx.ready(function(){
+            // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+        });
+        wx.error(function(res){
+            // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+        });
+    </script>
+    <script>
+        var share = {
+            desc: '雀巢主题日',
+            title: '主题日',
+            link: location.href,
+            imgUrl:'图片的外链'
+        }
+        wx.ready(function () {
+            // 分享给好友
+            wx.onMenuShareAppMessage({
+                title: share.title,
+                desc: share.desc,
+                link: share.link,
+                imgUrl:share.imgUrl
+            });
+            // 分享朋友圈
+            wx.onMenuShareTimeline({
+                title: share.title,
+                desc: share.desc,
+                link: share.link,
+                imgUrl: share.imgUrl
+            });
+        });
+    </script>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ jssdk.php
@@ -0,0 +1,114 @@
+<?php
+class JSSDK
+{
+    private $appId;
+    private $appSecret;
+
+    public function __construct($appId, $appSecret)
+    {
+        $this->appId = 'yourAppId';
+        $this->appSecret = 'yourAppSecret';
+    }
+
+    public function getSignPackage()
+    {
+        // 获取 getJsApiTicket的ticket
+        $jsapiTicket = $this->getJsApiTicket();
+        // url 是获取当前服务器的域名和请求参数
+        $url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        // 获取时间戳
+        $timestamp = time();
+        // 获取随机字符串
+        $nonceStr = $this->createNonceStr();
+
+        // 这里参数的顺序要按照 key 值 ASCII 码升序排序
+        $string = "jsapi_ticket=$jsapiTicket&noncestr=$nonceStr&timestamp=$timestamp&url=$url";
+    
+        // 使用sha1 加密
+        $signature = sha1($string);
+        // 生成一个数组 等调用
+        $signPackage = array(
+        "appId"     => $this->appId,
+        "nonceStr"  => $nonceStr,
+        "timestamp" => $timestamp,
+        "url"       => $url,
+        "signature" => $signature,
+        "rawString" => $string
+        );
+        return $signPackage;
+    }
+
+    //  生成长度16位的随机字符串
+    private function createNonceStr($length = 16)
+    {
+        $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        $str = "";
+        for ($i = 0; $i < $length; $i++) {
+            $str .= substr($chars, mt_rand(0, strlen($chars) - 1), 1);
+        }
+        return $str;
+    }
+
+    private function getJsApiTicket()
+    {
+        // jsapi_ticket 应该全局存储与更新，以下代码为将jsapi_ticket存在jsapi_ticket.txt文件中
+        // 文件位置 名称
+        $file = "jsapi_ticket.txt";
+        // 判断问文件是否存在
+        if (!file_exists($file)) {
+            // 获取accessToken
+            $accessToken = $this->getAccessToken();
+            $url = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&access_token=$accessToken";
+            // 使用httpGet请求该api
+            $res = json_decode($this->httpGet($url));
+            $ticket = $res->ticket;
+            if ($ticket) {
+                // 若获取到ticket 将数据写入文件
+                $wfile = fopen($file,"w") or die("unable to open file");
+                fwrite($wfile,$ticket);
+                fclose($wfile);
+            }
+        } else {
+            // 打开文件读取数据
+            $rfile = fopen($file,"r") or die("unable to open file");
+            $ticket = fread($rfile,filesize($file));
+        }
+        return $ticket;
+    }
+
+    private function getAccessToken()
+    {
+        // access_token 应该全局存储与更新，以下代码以写入到文件中做示例
+        // 同上获取ticket步骤
+        $file = "access_token.txt";
+        if (!file_exists($file)) {
+            $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$this->appId&secret=$this->appSecret";
+            $res = json_decode($this->httpGet($url));
+            $access_token = $res->access_token;
+            if ($access_token) {
+                $wfile = fopen($file,"w") or die("unable to open file");
+                fwrite($wfile,$access_token);
+                fclose($wfile);
+            }
+        } else {
+            $rfile = fopen($file,"r") or die("unable to open file");
+            $access_token = fread($rfile,filesize($file));
+        }
+        return $access_token;
+    }
+
+    // 请求接口方法
+    private function httpGet($url)
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); //不验证证书下同 
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false); // 过HTTPS验证
+        curl_setopt($curl, CURLOPT_TIMEOUT, 500);
+        curl_setopt($curl, CURLOPT_URL, $url);
+
+        $res = curl_exec($curl);
+        curl_close($curl);
+        return $res;
+    }
+}
